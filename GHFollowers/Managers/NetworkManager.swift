d41aa7eaefc7aaa3e361f7ffc1a1c9b12e3ce5c8
kovs705 <smartkovsik@gmail.com>
2023-01-13 15:18:50 +0300

    - Replaced Subviews into the Subviews folder - Changed some lines in NetworkManager to handle images cache storing - Added function for FollowerCells image to download image from url or take it from the cache if it already exists

--- GHFollowers/Managers/NetworkManager.swift
+++ GHFollowers/Managers/NetworkManager.swift
@@ -5,11 +5,12 @@
 //  Created by Kovs on 08.01.2023.
 //
 
-import Foundation
+import UIKit
 
 class NetworkManager {
     static let shared = NetworkManager()
-    let baseURL = "https://api.github.com/users/"
+    private let baseURL = "https://api.github.com/users/"
+    let cache = NSCache<NSString, UIImage>() // NSString as a key, UIImage as a value
     
     private init() {}
     
