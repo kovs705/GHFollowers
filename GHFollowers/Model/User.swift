ba541cfc01cf336c6db58348b38d4d4276f402e1
kovs705 <smartkovsik@gmail.com>
2023-01-17 03:38:02 +0300

    - Added HeaderVC for UserInfoVC - Added gettingUserInfo function - Refactored User struct, changed vars into lets - Added SecondaryTitleLabel

--- GHFollowers/Model/User.swift
+++ GHFollowers/Model/User.swift
@@ -9,20 +9,20 @@ import Foundation
 
 
 struct User: Codable {
-    var login: String
-    var avatarUrl: String
+    let login: String
+    let avatarUrl: String
     
     var name: String?
     var location: String?
     var bio: String?
     
-    var publicRepos: Int
-    var publicGists: Int
+    let publicRepos: Int
+    let publicGists: Int
     
-    var htmlUrl: String
+    let htmlUrl: String
     
-    var following: Int
-    var followers: Int
+    let following: Int
+    let followers: Int
     
-    var createdAt: String
+    let createdAt: String
 }
---
+++ GHFollowers/Utilites/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  GHFollowers
+//
+//  Created by Kovs on 17.01.2023.
+//
+
+import Foundation
+
+
+enum SFSymbols {
+    static let location = "mappin.and.ellipse"
+}
