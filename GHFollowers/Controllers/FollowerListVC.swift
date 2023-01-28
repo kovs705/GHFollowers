c544b4e5b06391314f5f66dd9571c762e0789390
kovs705 <smartkovsik@gmail.com>
2023-01-28 21:55:10 +0300

    - Added button as a rightBarButton for the future functions - Added new function in PersistenceManager for saving> adding and removing favourite users from Favourites screen - Added 2 new enum cases for GFErrors

--- GHFollowers/Controllers/FollowerListVC.swift
+++ GHFollowers/Controllers/FollowerListVC.swift
@@ -57,6 +57,13 @@ class FollowerListVC: UIViewController {
     func configureViewController() {
         view.backgroundColor = .systemBackground
         navigationController?.navigationBar.prefersLargeTitles = true
+        
+        let addButton = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addButtonTapped))
+        navigationItem.rightBarButtonItem = addButton
+    }
+
+    @objc func addButtonTapped() {
+        
     }
     
     func configureDataSource() {
---
+++ GHFollowers/Managers/PersistenceManager.swift
@@ -0,0 +1,88 @@
+//
+//  PersistenceManager.swift
+//  GHFollowers
+//
+//  Created by Kovs on 21.01.2023.
+//
+
+import Foundation
+
+enum PersistenceActionType {
+    case add, remove
+}
+
+enum PersistenceManager {
+    static private let defaults = UserDefaults.standard
+    
+    enum Keys {
+        static let favorites = "favorites"
+    }
+    
+    static func updateWith(favorite: Follower, actionType: PersistenceActionType, completed: @escaping (GFError?) -> Void) {
+        retrieveFavorites { result in
+            switch result {
+            case .success(let favorites):
+                
+                var retrievedFavorites = favorites
+                
+                switch actionType {
+                case .add:
+                    
+                    guard !retrievedFavorites.contains(favorites) else {
+                        // if contains:
+                        completed(.alreadyInFavorites)
+                        return
+                    }
+                    
+                    // if you didn't add this user yet:
+                    retrievedFavorites.append(favorite)
+                    
+                case .remove:
+                    retrievedFavorites.removeAll { $0.login == favorite.login}
+                }
+                
+                completed(save(favorites: favorites))
+                
+                
+            case .failure(let error):
+                completed(error)
+            }
+        }
+    }
+
+    static func retrieveFavorites(completed: @escaping (Result<[Follower], GFError>) -> Void) {
+        guard let favoritesData = defaults.object(forKey: Keys.favorites) as? Data else {
+            // if nil, so there's no favorite
+            completed(.success([]))
+            return
+        }
+        
+        // decode into followers array:
+        do {
+            let decoder = JSONDecoder()
+            let favorites = try decoder.decode([Follower].self, from: favoritesData)
+            
+            completed(.success(favorites))
+        } catch {
+            completed(.failure(.unableToFavorite))
+        }
+        
+    }
+    
+    static func save(favorites: [Follower]) -> GFError? { // GFError with ? because the result can be successfull
+        // encode:
+        do {
+            let encoder = JSONEncoder()
+            let encodedFavorites = try encoder.encode(favorites)
+            
+            // save to dafaults:
+            defaults.set(encodedFavorites, forKey: "favorites")
+            return nil
+        } catch {
+            return .unableToFavorite
+        }
+    }
+    
+    
+}
+
